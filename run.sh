718a9c7ee95868e6e8c28c68eb13ea73c247458b
Benjamin Henrion <zoobab@gmail.com>
2017-02-07 14:26:47 +0100

    add -v /dev/vboxnetctl:/dev/vboxnetctl and net=host

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v /dev/vboxdrv:/dev/vboxdrv -it vagrant-inside-docker:latest bash
+docker run -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl --net=host -it vagrant-inside-docker:latest bash
