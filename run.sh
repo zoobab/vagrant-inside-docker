7ab879f9b35c317344c9f11d6e704e9cfcbadb6b
Benjamin Henrion <zoobab@gmail.com>
2017-02-07 14:30:08 +0100

    add privileged

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl --net=host -it vagrant-inside-docker:latest bash
+docker run --privileged -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl --net=host -it vagrant-inside-docker:latest bash
