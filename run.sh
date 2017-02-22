3483e5d09f4233ace20fb110eaa48a14d0c38dbd
Benjamin Henrion <zoobab@gmail.com>
2017-02-22 09:05:58 +0100

    fix the run.sh to launch vagrant up and vagrant ssh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --privileged -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl -it vagrant-inside-docker:latest bash
+docker run --privileged -v /dev/vboxdrv:/dev/vboxdrv -v /dev/vboxnetctl:/dev/vboxnetctl -it vagrant-inside-docker:latest /bin/bash -c "vagrant up && vagrant ssh"
